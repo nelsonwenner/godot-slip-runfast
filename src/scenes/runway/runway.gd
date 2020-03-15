96a27bf5878b2c9bb70654ab4143f3770cfd06ea
unknown <nelsondiasdemedeiros@gmail.com>
2020-03-15 13:58:40 -0300

    refactor: refactoring some methods

--- src/scenes/runway/runway.gd
+++ src/scenes/runway/runway.gd
@@ -30,7 +30,7 @@ var lines = []
 var current_position = 0
 var lines_lenght
 
-var player_x = 0
+var camera_x = 0
 
 var speed = 0
 
@@ -38,13 +38,18 @@ var up_is_pressed = false
 
 var controller_draw_sprites = true
 
+var max_speed = 520
+
+var accumulate_curve = 0
+var distance_x = 0
+
 
 func _ready():
 	set_process_input(true)
 	init()
 	
 
-func _process(delta): 
+func _process(_delta): 
 	update()
 	
 	
@@ -54,34 +59,35 @@ func _draw():
 	
 	var number_position = 0
 	var start_point = (current_position / SEGMENT_LENGHT)
-	var cam_horizontal = (1500 + lines[start_point].get_y())
+	var cam_horizontal = (1800 + lines[start_point].get_world_y())
 	var max_y = HEIGHT
-	var x = 0
-	var distance_x = 0
+	
+	accumulate_curve  = 0
+	distance_x = 0
 	
 	for n in range(start_point, start_point + 300):
 		if (n >= lines_lenght): number_position = (lines_lenght * SEGMENT_LENGHT)
 		else: number_position = 0
 		
 		var line_instance = lines[n%lines_lenght]
-		var current_line = line_instance.screen_coordinates(player_x - x, cam_horizontal, current_position - number_position, CAMERA_DEPTH, RUNWAY_WIDTH, WIDTH, HEIGHT)
+		var current_line = line_instance.screen_coordinates(camera_x - accumulate_curve, cam_horizontal, current_position - number_position, CAMERA_DEPTH, RUNWAY_WIDTH, WIDTH, HEIGHT)
 		
-		var actual_position = lines[(n - 1) % lines_lenght]
+		var previous_line = lines[(n - 1) % lines_lenght]
 		
-		x += distance_x
+		accumulate_curve  += distance_x
 		distance_x += current_line.get_curve()
 		
 		current_line.set_clip(max_y)
 		
-		if (current_line.get_Y() >= max_y): continue
-		max_y = current_line.get_Y()
+		if (current_line.get_screen_y() >= max_y): continue
+		max_y = current_line.get_screen_y()
 		
 		add_colors(n)
 		
-		draw_runway(NEW_GRAMME, 0, actual_position.get_Y(), WIDTH, 0, current_line.get_Y(), WIDTH)
-		draw_runway(NEW_BORDER, actual_position.get_X(), actual_position.get_Y(), actual_position.get_W() * 1.2, current_line.get_X(), current_line.get_Y(), current_line.get_W() * 1.2)
-		draw_runway(NEW_RUNWAY, actual_position.get_X(), actual_position.get_Y(), actual_position.get_W(), current_line.get_X(), current_line.get_Y(), current_line.get_W())
-		draw_runway(NEW_DIVID_LINE, actual_position.get_X(), actual_position.get_Y(), actual_position.get_W() * 0.02, current_line.get_X(), current_line.get_Y(), current_line.get_W() * 0.02)
+		render_polygon(NEW_GRAMME, 0, previous_line.get_screen_y(), WIDTH, 0, current_line.get_screen_y(), WIDTH)
+		render_polygon(NEW_BORDER, previous_line.get_screen_x(), previous_line.get_screen_y(), previous_line.get_screen_w() * 1.2, current_line.get_screen_x(), current_line.get_screen_y(), current_line.get_screen_w() * 1.2)
+		render_polygon(NEW_RUNWAY, previous_line.get_screen_x(), previous_line.get_screen_y(), previous_line.get_screen_w(), current_line.get_screen_x(), current_line.get_screen_y(), current_line.get_screen_w())
+		render_polygon(NEW_DIVID_LINE, previous_line.get_screen_x(), previous_line.get_screen_y(), previous_line.get_screen_w() * 0.02, current_line.get_screen_x(), current_line.get_screen_y(), current_line.get_screen_w() * 0.02)
 	
 	draw_sprites()
 	update_position_sprites(start_point)
@@ -92,30 +98,21 @@ func init():
 		var struture_line = line.Line.new(0,0,0,0,0,0,0,0,0,0,0)
 		
 		lines.push_back(struture_line)
-		lines[index].set_z(index * SEGMENT_LENGHT)
+		lines[index].set_world_z(index * SEGMENT_LENGHT)
 		
 		controller_runway(index)
 	lines_lenght = lines.size()
 	set_process(true)
 	
-
-func controller_runway(index):
-	var new_tree_01 = tree_01.instance()
 	
-	if (index > 100 && index < 400 && index % 20 == 0):
-		lines[index].set_sprite_x(-0.5)
-		lines[index].set_sprite(new_tree_01)
-		
-	if (index > 500 && index % 20 == 0):
-		lines[index].set_sprite_x(1)
-		lines[index].set_sprite(new_tree_01)
-		
-	if (index > 300 && index < 750): lines[index].set_curve(0.5)
-	if (index > 800 && index < 1200): lines[index].set_curve(-0.7)
-	if (index > 1200): lines[index].set_curve(0.2)
+func _input(event):
+    if event.is_action_pressed("ui_up"):
+        up_is_pressed = true
+    elif event.is_action_released("ui_up"):
+        up_is_pressed = false
 
 
-func draw_runway(color, x1, y1, w1, x2, y2, w2):
+func render_polygon(color, x1, y1, w1, x2, y2, w2):
 	var point = [Vector2(int(x1 - w1), int(y1)), Vector2(int(x2 - w2), int(y2)),
 	Vector2(int(x2 + w2), int(y2)), Vector2(int(x1 + w1), int(y1))]
 	draw_primitive(PoolVector2Array(point), PoolColorArray([color, color, color, color, color]), PoolVector2Array([]))
@@ -144,7 +141,58 @@ func controller_position():
 		current_position -= (lines_lenght * SEGMENT_LENGHT)
 	while current_position < 0:
 		current_position += (lines_lenght * SEGMENT_LENGHT)
+		
 
+func controller_inputs():
+	if up_is_pressed:
+		
+		if Input.is_action_pressed("ui_right"):
+			camera_x += 150
+		if Input.is_action_pressed("ui_left"):
+			camera_x -= 150
+			
+		speed += 2
+	else:
+		speed -= 5
+		
+	speed = clamp(speed, 0, max_speed)
+	current_position += speed
+
+
+func controller_runway(index):
+	
+	var curves = {
+		CURVE_RIGHT00 = 1.5,
+		CURVE_RIGHT01 = 2.5,
+		CURVE_RIGHT02 = 3.5,
+		CURVE_RIGHT03 = 4.5,
+		CURVE_RIGHT04 = 5.5,
+		CURVE_RIGHT05 = 6.5,
+		CURVE_RIGHT06 = 7.5,
+		
+		CURVE_LEFT01 = -2.5,
+		CURVE_LEFT02 = -3.5,
+		CURVE_LEFT03 = -4.5,
+		CURVE_LEFT04 = -5.5,
+		CURVE_LEFT05 = -6.5,
+		CURVE_LEFT06 = -7.5
+	}
+	
+	var new_tree_01 = tree_01.instance()
+	
+	if (index > 100 && index < 400 && index % 20 == 0):
+		lines[index].set_sprite_x(-0.5)
+		lines[index].set_sprite(new_tree_01)
+		
+	if (index > 500 && index % 20 == 0):
+		lines[index].set_sprite_x(1)
+		lines[index].set_sprite(new_tree_01)
+		
+	if (index > 100 && index < 500): lines[index].set_curve(curves.CURVE_RIGHT01)
+	if (index > 500 && index < 1100): lines[index].set_curve(curves.CURVE_RIGHT00)
+	if (index > 1200): lines[index].set_curve(curves.CURVE_LEFT04)
+	if (index > 1600): lines[index].set_curve(curves.CURVE_LEFT03)
+	
 
 func add_colors(n):
 	if (n / 3) % 2:
@@ -159,32 +207,3 @@ func add_colors(n):
 	else:
 		NEW_DIVID_LINE = STRIPED_DIVID_LINE
 		NEW_GRAMME = STRIPED_GRAMME
-
-
-func controller_inputs():
-	if up_is_pressed:
-		speed += 3
-		current_position += speed
-		
-		if speed >= 600: speed = 600
-			
-		if current_position == 320000:
-			current_position = 0
-		
-		if Input.is_action_pressed("ui_right"):
-			player_x += 200
-		if Input.is_action_pressed("ui_left"):
-			player_x -= 200
-	
-	elif speed > 5: 
-		current_position += speed; speed -= 5
-		if speed < 0: current_position = 0 
-
-
-func _input(event):
-    if event.is_action_pressed("ui_up"):
-        up_is_pressed = true
-    elif event.is_action_released("ui_up"):
-        up_is_pressed = false
-
-
--- src/scripts/Line.gd
+++ src/scripts/Line.gd
@@ -2,48 +2,48 @@ class Line:
 	
 	var _scale; var _curve; var _sprite_x
 	
-	var _x; var _y; var _z
+	var _world_x; var _world_y; var _world_z
 	
-	var _X; var _Y; var _W
+	var _screen_x; var _screen_y; var _screen_w
 	
 	var _clip; var _sprite
 	
-	func _init(x, y, z, X, Y, W, scale, curve, sprite_x, clip, sprite):
-		self._x = x
-		self._y = y
-		self._z = z
-		self._X = X
-		self._Y = Y
-		self._W = W
+	func _init(world_x, world_y, world_z, screen_x, screen_y, screen_w, scale, curve, sprite_x, clip, sprite):
+		self._world_x = world_x
+		self._world_y = world_y
+		self._world_z = world_z
+		self._screen_x = screen_x
+		self._screen_y = screen_y
+		self._screen_w = screen_w
 		self._clip = clip
 		self._scale = scale
 		self._curve = curve
 		self._sprite = sprite
 		self._sprite_x = sprite_x
 		
+	
+	func get_world_x():
+		return self._world_x
 		
-	func get_z():
-		return self._z
+		
+	func get_world_y():
+		return self._world_y
 	
 	
-	func get_x():
-		return self._x
+	func get_world_z():
+		return self._world_z	
 		
 		
-	func get_y():
-		return self._y
+	func get_screen_x():
+		return self._screen_x
 		
 		
-	func get_X():
-		return self._X
+	func get_screen_y():
+		return self._screen_y
 		
 		
-	func get_Y():
-		return self._Y
-		
-		
-	func get_W():
-		return self._W
+	func get_screen_w():
+		return self._screen_w
 		
 		
 	func get_scale():
@@ -66,28 +66,28 @@ class Line:
 		return self._sprite	
 	
 	
-	func set_z(value):
-		self._z = value
+	func set_world_z(value):
+		self._world_z = value
 	
 	
-	func set_x(value):
-		self._x = value
+	func set_world_x(value):
+		self._world_x = value
 		
 		
-	func set_y(value):
-		self._y = value
+	func set_world_y(value):
+		self._world_y = value
 		
 		
-	func set_X(value):
-		self._X = value
+	func set_screen_x(value):
+		self._screen_x = value
 		
 		
-	func set_Y(value):
-		self._Y = value
+	func set_screen_y(value):
+		self._screen_y = value
 		
 		
-	func set_W(value):
-		self._W = value
+	func set_screen_w(value):
+		self._screen_w = value
 		
 		
 	func set_scale(value):
@@ -111,19 +111,19 @@ class Line:
 		
 		
 	func screen_coordinates(cam_x, cam_y, cam_z, camera_depth, road_width, width, height):
-		self._scale = camera_depth / ((self._z - cam_z) if (self._z - cam_z) > 0 else 0.01)
-		self._X = ((1 + self._scale * (self._x - cam_x)) * width / 2)
-		self._Y = ((1 - self._scale * (self._y - cam_y)) * height / 2)
-		self._W = (self._scale * road_width * (width / 2))
+		self._scale = camera_depth / ((self._world_z - cam_z) if (self._world_z - cam_z) > 0 else 0.01)
+		self._screen_x = ((1 + self._scale * (self._world_x - cam_x)) * width / 2)
+		self._screen_y = ((1 - self._scale * (self._world_y - cam_y)) * height / 2)
+		self._screen_w = (self._scale * road_width * (width / 2))
 		return self
 	
 	
 	func run_sprite():
 		
-		var offsetX = self._X + self._scale * self._sprite_x * 1920 / 2
-		var offsetY = self._Y + 4
-		var offsetW = 650 * self._W / 266
-		var offsetH = 520 * self._W / 266
+		var offsetX = self._screen_x + self._scale * self._sprite_x * 1920 / 2
+		var offsetY = self._screen_y + 4
+		var offsetW = 650 * self._screen_w / 266
+		var offsetH = 520 * self._screen_w / 266
 		
 		offsetX += offsetW * self._sprite_x
 		offsetY += offsetH * (-1)
